948f5d3c9396406860cfca4794fc9e0e008ea1e5
Giovdey <174440@unsaac.edu.pe>
2023-04-25 13:06:59 -0500

    Component

---
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -0,0 +1,35 @@
+import { ProductButtons, ProductCard, ProductImage, ProductTitle } from "../components/ProductCard"
+
+const product={
+    id:'1',
+    title:'Coffe',
+    img:'./coffee-mug.png'
+
+}
+export const ShoppingPage = () => {
+  return (
+    <div>
+        <h1>Shopping Store</h1>
+        <hr/>
+        <div style={{
+            display:"flex",
+            flexDirection:"row",
+            flexWrap:"wrap",
+            padding:2
+        }}>
+            <ProductCard product={product}>
+                <ProductCard.Image/>
+                <ProductCard.Title />
+                <ProductCard.Buttons/>
+            </ProductCard>
+
+            <ProductCard product={product}>
+                <ProductImage/>
+                <ProductTitle/>
+                <ProductButtons/>
+            </ProductCard>
+            
+        </div>
+    </div>
+  )
+}
