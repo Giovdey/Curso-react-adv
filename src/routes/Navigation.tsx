8514640f0542d50b02bfc5e47cb16b8ab45b844d
Giovdey <174440@unsaac.edu.pe>
2023-04-25 11:24:40 -0500

    Configuración proyecto 2da parte

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -2,58 +2,44 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  NavLink,
-  Redirect
+  NavLink
 } from 'react-router-dom';
 
 import logo from '../logo.svg';
 
-// import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
-import { routes } from './routes';
-import { Suspense } from 'react';
-
 export const Navigation = () => {
   return (
-    <Suspense fallback={<span>Loading</span>}>
-      <Router>
+    <Router>
       <div className="main-layout">
-      <nav>
-          <img src={ logo } alt="React Logo" />
-        <ul>
-          {/* Rutas dinámicas */}
-          {
-            routes.map(({path,name})=>(
-              <li key={path}>
-                <NavLink 
-                  to={path} 
-                  activeClassName="nav-active" 
-                  exact
-                >
-                  {name}
-                </NavLink>
-              </li>
-              ))
-          }
-        </ul>
-      </nav>
+        <nav>
+            <img src={ logo } alt="React Logo" />
+          <ul>
+            <li>
+              <NavLink to="/" activeClassName="nav-active" exact>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
+            </li>
+          </ul>
+        </nav>
 
-      {/* A <Switch> looks through its children <Route>s and
-          renders the first one that matches the current URL. */}
-      <Switch>
-      {
-        routes.map(({path,component:Component})=>(
-        <Route path={path}
-          key={path}
-          render={()=>{
-            return <Component/>
-          }}
-        />
-        ))
-      }
-      <Redirect to={routes[0].path}/>
-      </Switch>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <h1>About</h1>
+          </Route>
+          <Route path="/users">
+            <h1>Users</h1>
+          </Route>
+          <Route path="/">
+            <h1>Home</h1>
+          </Route>
+        </Switch>
       </div>
-      </Router>
-    </Suspense>
+    </Router>
   );
 }
